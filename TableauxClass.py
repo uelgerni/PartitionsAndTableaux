0cf9f815e6b70f4289718e9a30d10046bcec78dc
uelgerni <https://uelgerni@github.com>
2020-06-17 17:02:55 +0200

    some cleanup

--- TableauxClass.py
+++ TableauxClass.py
@@ -9,24 +9,20 @@ basic tableaux class for multiplication
 
 class Tableaux(arr):
 
-
-    def __new__(cls, input_array): # creating an ndarray from a list or similar object and casting it to Tableaux type
+    def __new__(cls, input_array):  # creating an ndarray from a list or similar object and casting it to Tableaux type
         # print('In __new__ with class %s' % cls)
         obj = np.asarray(input_array).view(cls)
         return obj
 
     def __mul__(self, other):  # carries out Multiplication of tableaux using the sliding operation
 
-    # transforming self, other to be the correct shape for the algorithm
+        # transforming self, other to be the correct shape for the algorithm
         a = np.asarray([self])
-        print(a)
         b = []
-
         for i in other:
             b.append([i])
         b = np.asarray(b)
 
-        print(b)
         c = (-1) * np.ones((len(b), len(a.T)))
         d = np.zeros((len(a), len(b.T)))
         e = np.concatenate((np.concatenate((c, b), axis=1), np.concatenate((a, d), axis=1)),
@@ -43,6 +39,6 @@ class Tableaux(arr):
 
 
 a = Tableaux([1, 5, 3, 5])
-b = Tableaux(([1, 4, 3]))
+b = Tableaux([1, 4, 3])
 c = a * b
 print(c)
