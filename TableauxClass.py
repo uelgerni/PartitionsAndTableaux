e184867d5ce4140c77feb81515feed323affaba2
uelgerni <https://uelgerni@github.com>
2020-06-17 16:45:33 +0200

    use T1 * T2 to multiplicate 2 tableaux(s?) also you can actually create tableaux without creating ndarrays and casting them

--- TableauxClass.py
+++ TableauxClass.py
@@ -1,66 +1,45 @@
 from Sliding import *
 
-class Tableaux():
+arr = np.ndarray
 
-    def TableauxGenerator(self, diagram, max, liste=[(0, 0)], next=2):
-        diagram[0][0] = 1
+"""
+basic tableaux class for multiplication
+"""
 
-        if next > max:
-            yield diagram
-        else:
-            for i in range(len(diagram)):
-                for j in range(len(diagram[i])):
-                    if diagram[i][j] == -1 and (i, j) not in liste:
-                        if (i, j - 1) in liste or j == 0:
-                            if (i - 1, j) in liste or i == 0:
-                                a = np.copy(diagram)  # to create deepcopy
-                                a[i][j] = next
-                                for k in self.TableauxGenerator(a, max, liste + [(i, j)], next + 1):
-                                    yield k
 
-    def diegramprep(self, d):
-        diagram = np.concatenate((np.concatenate((d, np.zeros((len(d), 2))), axis=1), np.zeros((2, len(d.T) + 2))),
-                                 axis=0)  # add rows and columns with zeros
-        max = (-1) * np.sum(diagram)
-        return (diagram, max)
+class Tableaux(arr):
 
-    def __init__(self, d):
-        diagram = self.diegramprep(d)
-        for p in self.TableauxGenerator(diagram[0], diagram[1]):
-            p = np.delete(p, (-1), axis=1)
-            p = np.delete(p, (-1), axis=0)
-            p = np.delete(p, (-1), axis=1)
-            p = np.delete(p, (-1), axis=0)
-            print(p)
+    def __new__(cls, input_array):
+        # print('In __new__ with class %s' % cls)
+        obj = np.asarray(input_array).view(cls)
+        return obj
 
-    def __mul__(self, other):
+    def __array_finalize__(self, obj):
+        # see InfoArray.__array_finalize__ for comments
+        if obj is None: return
+        self.info = getattr(obj, 'info', None)
+
+    def __mul__(self, other):  # carries out Multiplication of tableaux using the sliding operation
+
+        # a = [self]
+        # b[i] =
         c = (-1) * np.ones((len(other), len(self.T)))
         d = np.zeros((len(self), len(other.T)))
-        e = np.concatenate((np.concatenate((c, other), axis=1), np.concatenate((self, d), axis=1)), axis=0)
+        e = np.concatenate((np.concatenate((c, other), axis=1), np.concatenate((self, d), axis=1)),
+                           axis=0)  # constructs skewtableau from a and b as explaint in W.Fultons book
         stableau = np.concatenate((np.concatenate((e, np.zeros((len(e), 2))), axis=1), np.zeros((2, len(e.T) + 2))),
-                                  axis=0)
-        while stableau[0][0] == -1:
+                                  axis=0)  # adds two rows and columns of zeros to avoid indexerror in sliding
+        while stableau[0][0] == -1:  # does sliding operation until the result is a youngtableau
             stableau = np.copy(Sliding(stableau))
         stableau = np.delete(stableau, (-1), axis=1)
         stableau = np.delete(stableau, (-1), axis=0)
-        return stableau
-
-    def mult(a, b):
-        c = (-1) * np.ones((len(b), len(a.T)))
-        d = np.zeros((len(a), len(b.T)))
-        e = np.concatenate((np.concatenate((c, b), axis=1), np.concatenate((a, d), axis=1)), axis=0)
-        stableau = np.concatenate((np.concatenate((e, np.zeros((len(e), 2))), axis=1), np.zeros((2, len(e.T) + 2))),
-                                  axis=0)
-        while stableau[0][0] == -1:
-            stableau = np.copy(Sliding(stableau))
         stableau = np.delete(stableau, (-1), axis=1)
-        stableau = np.delete(stableau, (-1), axis=0)
+        stableau = np.delete(stableau, (-1), axis=0)  # deletes rows and columns of zeros
         return stableau
 
-a = np.array([[1,2,3]])
-b = np.array(([[1],[2],[3]]))
 
-c= a * b
-print (c)
 
-c = a.mult(b)
+a = Tableaux([[1, 5, 3,5]])
+b = Tableaux(([[1], [4], [3]]))
+c = a * b
+print(c)
--- multiplication.py
+++ multiplication.py
@@ -15,8 +15,8 @@ def mult(a,b):          #carries out Multiplication of tableaux using the slidin
     return stableau
 
 
-a = np.array([[1,2,3]])
-b = np.array(([[1],[2],[3]]))
+a = np.array([[1,5,3,5]])
+b = np.array(([[1],[4],[3]]))
 
 c=mult(a,b)
 print (c)
